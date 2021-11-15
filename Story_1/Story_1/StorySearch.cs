977f534ced260ab4fd109b62ab36ef31b93cdfd9
Pavithra2304 <92969170+Pavithra2304@users.noreply.github.com>
2021-11-15 18:44:01 +0530

    Add files via upload

---
+++ Story_1/Story_1/StorySearch.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Story_1
+{
+    class StorySearch
+    {
+       
+        
+            public List<Story> findStory(List<Story> jobList, String authorname)
+            {
+
+                List<Story> result = new List<Story>();
+                foreach (var item in jobList)
+                {
+                    if (item.Authorname.Equals(authorname))
+                    {
+
+                        result.Add(item);
+
+                    }
+                }
+
+                return result;
+
+            }
+            public List<Story> findStory(List<Story> jobList, int noOfLikes)
+
+            {
+                List<Story> result = new List<Story>();
+                foreach (var item in jobList)
+                {
+                    if (item.NoOfLikes >= noOfLikes)
+                    {
+                        result.Add(item);
+                    }
+                }
+                return result;
+            }
+
+        
+    }
+}
